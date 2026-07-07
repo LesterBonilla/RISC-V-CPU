736c23286fbd88f3c67da6f6c2423886bd7d7065
Lester <lester.bonilla@outlook.com>
2026-07-07 10:58:47 -0700

    Define if_id_reg_t and id_ex_reg_t structs

--- rtl/pipeline_pkg.sv
+++ rtl/pipeline_pkg.sv
@@ -9,11 +9,38 @@ package pipeline_pkg;
     } fwd_sel_e;
 
     typedef struct packed {
-        
-    } id_ex_ctrl_t;
+        // Data lines
+        logic [31:0]    instruction;
+        logic [31:0]    pc;
+        logic [31:0]    pc_plus4;
+
+    } if_id_reg_t;
 
     typedef struct packed {
-        
-    } id_ex_data_t;
+        // Control signals
+        logic               reg_write;
+        logic               jump;
+        logic               branch;
+        alu_op_e            alu_op;
+        alu_src_a_e         alu_src_a;
+        alu_src_b_e         alu_src_b;
+        wb_src_e            wb_src;
+        branch_op_e         branch_op;
+        load_op_e           load_op;
+        store_op_e          store_op;
+        imm_src_e           imm_src;
+        target_adder_src_e  target_adder_src;
+
+        // Data lines
+        logic [4:0]         rs1;
+        logic [4:0]         rs2;
+        logic [4:0]         rd;
+        logic [31:0]        imm_extended;
+        logic [31:0]        pc;
+        logic [31:0]        pc_plus4;
+        logic [31:0]        reg_data_1;
+        logic [31:0]        reg_data_2;
+
+    } id_ex_reg_t;
 
 endpackage
