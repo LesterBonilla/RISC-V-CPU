8ebbb678e161f1194d314edd000833753809fda2
Lester <lester.bonilla@outlook.com>
2026-07-07 17:27:12 -0700

    Add pipeline register module

---
+++ rtl/pipeline_register.sv
@@ -0,0 +1,26 @@
+module pipeline_register #(
+    parameter type T = logic [31:0]
+)(
+    input logic clk,
+    input logic rst_n,
+    input logic en,
+    input logic flush,
+
+    input T     data_in,
+
+    output T    data_out
+);
+
+    always_ff @(posedge clk or negedge rst_n) begin
+        if (!rst_n) begin
+            data_out <= '0;
+            
+        end else if (flush) begin // TODO: Decide if this should affect just the valid bit
+            data_out <= '0;
+        
+        end else if (en) begin
+            data_out <= data_in;
+        end
+    end
+    
+endmodule
