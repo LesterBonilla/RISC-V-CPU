09fba077080612893ccc8474c70bd08ee80da516
Lester <lester.bonilla@outlook.com>
2026-07-08 11:37:32 -0700

    Add write_mask support to data_memory

--- rtl/data_memory.sv
+++ rtl/data_memory.sv
@@ -1,14 +1,16 @@
 module data_memory #(
     parameter int SIZE = 1024,
     parameter int XLEN = 32,
-    localparam int ADDR_WIDTH = $clog2(SIZE)
+    localparam int ADDR_WIDTH = $clog2(SIZE),
+    localparam int NUM_BYTES = (XLEN/8)
 )(
     input logic     clk,
     input logic     write_en,
 
-    input logic [ADDR_WIDTH-1:0] write_address,
     input logic [ADDR_WIDTH-1:0] read_address,
+    input logic [ADDR_WIDTH-1:0] write_address,
     input logic [XLEN-1:0]       write_data,
+    input logic [NUM_BYTES-1:0]  write_mask,
 
     output logic [XLEN-1:0] data_out
 );
@@ -19,8 +21,12 @@ module data_memory #(
     assign data_out = memory[read_address];
 
     always_ff @(posedge clk) begin
-        if (write_en)
-            memory[write_address] <= write_data;
+        if (write_en) begin
+            if (write_mask[0]) memory[write_address][7:0]   <= write_data[7:0];
+            if (write_mask[1]) memory[write_address][15:8]  <= write_data[15:8];
+            if (write_mask[2]) memory[write_address][23:16] <= write_data[23:16];
+            if (write_mask[3]) memory[write_address][31:24] <= write_data[31:24];
+        end
     end
     
 endmodule
