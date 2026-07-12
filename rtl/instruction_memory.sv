7c03cd7508b7a11eabff293e334b78d12302b605
Lester <lester.bonilla@outlook.com>
2026-07-12 11:01:06 -0700

    Have data and instruction memories pull from same mem.hex

--- rtl/instruction_memory.sv
+++ rtl/instruction_memory.sv
@@ -8,7 +8,7 @@ module instruction_memory #(
     localparam int ADDR_WIDTH_BYTES = $clog2(SIZE_WORDS * 4);
 
     logic [31:0] memory [SIZE_WORDS-1:0];
-    initial $readmemh("imem.hex", memory);
+    initial $readmemh("mem.hex", memory);
 
     assign data_out = memory[address[ADDR_WIDTH_BYTES-1:2]];
     
