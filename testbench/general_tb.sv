c76f934da38c9c9551a6a40fb61d7c00e0c53595
Lester <lester.bonilla@outlook.com>
2026-07-15 07:07:17 -0700

    Rename core_tb to general_tb

---
+++ testbench/general_tb.sv
@@ -0,0 +1,42 @@
+`timescale 1ns/1ps
+
+module general_tb;
+
+    logic clk;
+    logic rst_n;
+    logic test_done;
+
+    localparam logic [31:0] TEST_STATUS_ADDR = 32'hCAFE0000;
+
+    core # (.IMEM_SIZE_WORDS(1024*256), .DMEM_SIZE_WORDS(1024*256)) dut (
+        .clk    (clk),
+        .rst_n  (rst_n)
+    );
+
+    initial begin
+        clk = 1'b0;
+        forever #5 clk = ~clk;
+    end
+
+    initial begin
+        rst_n = 1'b0;
+        #20;
+        rst_n = 1'b1;
+    end
+
+    initial begin
+        $readmemh("../tests/I-jalr-00.hex",dut.imem_inst.memory);
+        $readmemh("../tests/I-jalr-00.hex",dut.dmem_inst.memory);
+    end
+
+    assign test_done = (dut.dmem_inst.address == TEST_STATUS_ADDR && dut.dmem_inst.write_en == 1'b1);
+
+    always_ff @(posedge clk) begin
+        if (test_done) begin
+            if (dut.dmem_inst.data_in == 32'd1) $display("TEST PASSED");
+            else                                $display("TEST FAILED");
+            $stop;
+        end
+    end
+
+endmodule
