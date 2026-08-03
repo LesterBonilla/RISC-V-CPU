1d2f79ceb169b6c1ed786c2983ce1d319a8d50ca
Lester <lester.bonilla@outlook.com>
2026-08-03 14:54:39 -0700

    Adapt uart_tb to handle uart_rx module as DUT

--- testbench/uart_tb.sv
+++ testbench/uart_tb.sv
@@ -1,10 +1,11 @@
 module uart_tb;
 
 //------------------------------------------------------------------------------
-// Clock and Reset
+// Clock, Reset, Baud Generator
 //------------------------------------------------------------------------------
     logic clk = 0;
     logic rst_n = 0;
+    logic tick_16x = 0;
 
     always #5 clk = ~clk;
 
@@ -15,18 +16,40 @@ module uart_tb;
         @(posedge clk);
     endtask
 
+    task automatic generate_tick_16x();
+        forever begin
+            @(posedge clk);
+            tick_16x = 1;
+            @(posedge clk);
+            tick_16x = 0;
+        end
+    endtask
+
+    initial generate_tick_16x();
+
 //------------------------------------------------------------------------------
 // DUT
 //------------------------------------------------------------------------------
-    logic       rx_pin, read_en;
-    logic [7:0] data_out;
+    localparam int unsigned FIFO_WIDTH = 8;
+    localparam int unsigned FIFO_DEPTH = 16;
 
-    uart dut (
-        .clk        (clk),
-        .rst_n      (rst_n),
-        .rx_pin     (rx_pin),
-        .read_en    (read_en),
-        .data_out   (data_out)
+    logic rx_pin, rx_fifo_rd_en, rx_fifo_flush;
+    logic rx_fifo_empty, rx_fifo_full;
+
+    logic [$clog2(FIFO_DEPTH):0]    rx_fifo_count;
+    logic [FIFO_WIDTH-1:0]          data_out;
+
+    uart_rx #(.FIFO_DEPTH(FIFO_DEPTH), .FIFO_WIDTH(FIFO_WIDTH)) dut (
+        .clk                (clk),
+        .rst_n              (rst_n),
+        .tick_16x           (clk),
+        .rx_pin             (rx_pin),
+        .rx_fifo_rd_en      (rx_fifo_rd_en),
+        .rx_fifo_flush      (rx_fifo_flush),
+        .rx_fifo_empty      (rx_fifo_empty),
+        .rx_fifo_full       (rx_fifo_full),
+        .rx_fifo_count      (rx_fifo_count),
+        .rx_fifo_data_out   (data_out)
     );
 
 //------------------------------------------------------------------------------
@@ -44,15 +67,19 @@ module uart_tb;
         end
     endtask
 
-    task automatic reset_queue();
+    task automatic reset_test();
         write_queue.delete();
+        rx_pin  = 1;
+        rx_fifo_rd_en = 0;
+        rx_fifo_flush = 0;
+        reset_dut();
     endtask
 
     task automatic read_uart();
         // Read values from the UART, check against the write_queue
         int size = write_queue.size();
         for (int i = 0; i < size; i++) begin
-            read_en     = 1'b1;
+            rx_fifo_rd_en = 1'b1;
             queue_data  = write_queue.pop_front();
             if (queue_data != data_out) 
                 $error("MISMATCH data_out:\nGot: %0h\nExpected: %0h", data_out, queue_data);
@@ -60,7 +87,7 @@ module uart_tb;
                 $display("Read %0h from UART", data_out);
             @(posedge clk); #0.5;
         end
-        read_en = 0;
+        rx_fifo_rd_en = 0;
     endtask
 
     task automatic write_uart(logic [7:0] data);
@@ -95,18 +122,15 @@ module uart_tb;
     task automatic test_writing_one(logic [7:0] data);
         test = "WRITING ONE";
         $display("\n=== TEST: Writing value %0h ===", data);
-        reset_queue();
-        reset_dut();
-        read_en = 0;
+        reset_test();
         write_uart(data);
         read_uart();    
     endtask
 
     task automatic test_write_many();
         $display("\n=== TEST: Write many ===");
-        reset_queue();
-        read_en = 0;
-        reset_dut();
+        reset_test();
+
         for (int i = 0; i < 16; i++) begin
             $display("Writing %0h to UART", i);
             write_uart(i);
@@ -118,10 +142,7 @@ module uart_tb;
 // Start tests
 //------------------------------------------------------------------------------
     initial begin
-        force dut.divisor_r = 1;
-        rx_pin  = 1;
-        read_en = 0;
-        reset_dut();
+        reset_test();
         test_writing_one(8'h77);
         test_write_many();
         $finish;
