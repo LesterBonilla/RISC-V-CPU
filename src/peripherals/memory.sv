cc2e7c3f80f290472f9f590b4435febc5eeed312
Lester <lester.bonilla@outlook.com>
2026-08-07 13:07:32 -0700

    Change memory module to have synchronous reads to infer block ram

--- src/peripherals/memory.sv
+++ src/peripherals/memory.sv
@@ -10,6 +10,7 @@ module memory # (
     input  logic [31:0]     data_in,
     input  logic [3:0]      byte_en,
     input  logic            write_en,
+    input  logic            read_en,
     output logic [31:0]     dmem_data
 );
 
@@ -22,9 +23,6 @@ module memory # (
 
     assign imem_word_address = imem_address[ADDR_WIDTH_BYTES-1:2];
     assign dmem_word_address = dmem_address[ADDR_WIDTH_BYTES-1:2];
-
-    assign imem_data = memory[imem_word_address];
-    assign dmem_data = memory[dmem_word_address];
     
     always_ff @(posedge clk) begin
         if (write_en) begin
@@ -32,6 +30,10 @@ module memory # (
             if (byte_en[1]) memory[dmem_word_address][1] <= data_in[15:8];
             if (byte_en[2]) memory[dmem_word_address][2] <= data_in[23:16];
             if (byte_en[3]) memory[dmem_word_address][3] <= data_in[31:24]; 
+        end
+        if (read_en) begin
+            imem_data <= memory[imem_word_address];
+            dmem_data <= memory[dmem_word_address];
         end
     end
 
